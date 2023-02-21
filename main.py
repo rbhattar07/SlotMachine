d398931bb3ff68f872df0e03684c6fff7238042a
Riddh Bhattar <riddhbhattar@gmail.com>
2023-02-21 16:41:51 -0600

    Final Game V1.

--- main.py
+++ main.py
@@ -108,12 +108,11 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
-        total_bet = bet*lines
+        total_bet = bet * lines
 
         if total_bet > balance:
             print(f"You do not have enough to bet on that amount. Your current balance is ${balance}")
@@ -127,5 +126,19 @@ def main():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
     print(f"You won on lines:", * winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+
+
 
 main()
